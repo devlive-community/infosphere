30904442a7219680f8c3bf1303205ac895b8fc6b
qianmoQ <shicheng@devlive.org>
2024-07-07 01:38:22 +0800

    移除老版 UI

--- core/infosphere-server/src/main/resources/static/js/entity/article.js
+++
@@ -1,7 +0,0 @@
-function ArticleEntity(id, title, content, published, tags) {
-    this.id = id;
-    this.title = title;
-    this.content = content;
-    this.published = published;
-    this.tags = tags
-}
--- core/infosphere-server/src/main/resources/static/js/entity/editor.js
+++
@@ -1,8 +0,0 @@
-function EditorEntity(id, markdown, tocContainer, mode, config, toolbarIcons) {
-    this.id = id; // 编辑器ID
-    this.markdown = markdown; // 编辑器内容
-    this.tocContainer = tocContainer; // 编辑器内容菜单
-    this.mode = mode;
-    this.toolbarIcons = toolbarIcons;
-    this.config = config; // 编辑器配置
-}
