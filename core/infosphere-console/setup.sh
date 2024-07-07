9b982cd97d3651d4e2ac80e7b34b3af68841dfae
qianmoQ <shicheng@devlive.org>
2024-07-07 11:05:02 +0800

    添加 UI 模块

---
+++ core/infosphere-console/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "========== Running setup script =========="
+
+echo "Installing Shadcn Vue for button component"
+yes | npx shadcn-vue@latest add button
+
+echo "========== Done setup script =========="
---
+++ core/infosphere-console/src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './assets/index.css'
+import App from './App.vue'
+
+const app = createApp(App)
+app.mount('#app')
