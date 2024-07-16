6f0787ddb7f29de214dd68442fefb6f764241568
qianmoQ <shicheng@devlive.org>
2024-07-16 19:06:12 +0800

    添加 Tree 组件

---
+++ core/infosphere-console/src/views/components/tree/DefaultTree.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="w-full">
+    <div v-for="item in items" :key="item.key">
+      <TreeNode :item="item" :selectedKey="selectedKey" @select-item="selectItem">
+        <template v-if="$slots.node" #node="{ node }">
+          <slot name="node" :node="node"/>
+        </template>
+      </TreeNode>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Tree } from '@/views/components/tree/Tree.ts'
+import TreeNode from '@/views/components/tree/TreeNode.vue'
+
+export default defineComponent({
+  name: 'DefaultTree',
+  components: { TreeNode },
+  props: {
+    items: {
+      type: Array<Tree>,
+      required: true
+    },
+    selectedKey: {
+      type: String as () => string | null | undefined,
+      required: false
+    }
+  },
+  methods: {
+    selectItem(key: string)
+    {
+      this.$emit('select-item', key)
+    }
+  }
+})
+</script>
