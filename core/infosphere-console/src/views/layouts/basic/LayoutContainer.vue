05cea1ceb64c5467665a2afc6601e5a6fe148ba8
qianmoQ <shicheng@devlive.org>
2024-07-19 10:29:54 +0800

    增加底部布局

--- core/infosphere-console/src/views/layouts/basic/LayoutContainer.vue
+++ core/infosphere-console/src/views/layouts/basic/LayoutContainer.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="hidden flex-col md:flex">
+  <div class="hidden flex-col md:flex min-h-screen">
     <LayoutHeader/>
     <div class="flex-1 space-y-4 min-h-[700px]">
       <RouterView/>
     </div>
+    <LayoutFooter/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import LayoutHeader from '@/views/layouts/basic/components/LayoutHeader.vue'
+import LayoutFooter from '@/views/layouts/basic/components/LayoutFooter.vue'
 
 export default defineComponent({
   name: 'LayoutContainer',
-  components: { LayoutHeader }
+  components: { LayoutFooter, LayoutHeader }
 })
 </script>
