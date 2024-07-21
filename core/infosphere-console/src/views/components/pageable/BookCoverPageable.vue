09c73a83732de0e372f442141d6b5b63f9ce4303
qianmoQ <shicheng@devlive.org>
2024-07-21 09:14:07 +0800

    添加书籍状态展示

--- core/infosphere-console/src/views/components/pageable/BookCoverPageable.vue
+++ core/infosphere-console/src/views/components/pageable/BookCoverPageable.vue
@@ -5,7 +5,13 @@
         <RouterLink :to="`/book/info/${item.identify}`">
           <InfoSphereTooltip position="top">
             <template #title>
-              <div class="w-full h-48">
+              <div class="w-full h-48 relative">
+                <div class="absolute top-0 right-0 pl-1 pr-1 -mt-0.5 m-0.5 bg-white bg-opacity-30 rounded">
+                  <span v-if="item.state === 'STARTED'" class="text-xs text-cyan-600 font-bold">开始中</span>
+                  <span v-else-if="item.state === 'PAUSED'" class="text-xs text-amber-600 font-bold">暂停中</span>
+                  <span v-else-if="item.state === 'STOPPED'" class="text-xs text-red-600 font-bold">已停止</span>
+                  <span v-else-if="item.state === 'FINISHED'" class="text-xs text-green-600 font-bold">已完成</span>
+                </div>
                 <AspectRatio class="w-full h-full">
                   <img :src="item.cover ? item.cover : '/static/images/default-cover.png'" :alt="item.name" class="rounded-md w-full h-full border-2"/>
                 </AspectRatio>
