75fc2aba06561230076cda22b6ec50ae55415285
qianmoQ <shicheng@devlive.org>
2024-08-19 20:59:23 +0800

    增加软件基本描述

--- core/infosphere-server/src/main/java/org/devlive/infosphere/server/InfoSphereBanner.java
+++ core/infosphere-server/src/main/java/org/devlive/infosphere/server/InfoSphereBanner.java
@@ -5,22 +5,26 @@ import org.springframework.boot.Banner;
 import org.springframework.core.env.Environment;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Slf4j
 public class InfoSphereBanner
         implements Banner
 {
+
     @Override
     public void printBanner(Environment environment, Class<?> sourceClass, PrintStream out)
     {
-        String banner = String.join(File.separator, environment.getProperty("spring.config.location"), "banner.txt");
-        try (FileReader reader = new FileReader(banner)) {
+        String bannerPath = String.join(File.separator, environment.getProperty("spring.config.location"), "banner.txt");
+        try (InputStreamReader reader = new InputStreamReader(Files.newInputStream(Paths.get(bannerPath)), StandardCharsets.UTF_8)) {
             int ch;
             while ((ch = reader.read()) != -1) {
-                out.print(ch);
+                out.print(Character.toChars(ch));
             }
         }
         catch (IOException e) {
