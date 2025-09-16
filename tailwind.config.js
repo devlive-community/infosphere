732cf07348bf632a640f01d7e2ef3c5df5e30852
qianmoQ <shicheng@devlive.org>
2025-09-17 01:43:00 +0800

    feat: 增加 tailwind 配置

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './frontend/views/**/*.ejs',
+        './frontend/**/*.js',
+        './frontend/js/**/*.js'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: []
+}
