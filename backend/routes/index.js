3ea49046caf617fd168eb7fe18302902c1184722
qianmoQ <shicheng@devlive.org>
2025-09-20 19:09:34 +0800

    feat: 添加首页模板

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const { asyncHandler } = require('../middleware/async-handler')
+const Book = require('../models/book')
 const router = express.Router()
 
 router.get('/', asyncHandler(async (req, res) => {
-    res.render('pages/index')
+    const [stats, hotBooks] = await Promise.all([
+        Book.summaryByUser(),
+        Book.findTop6ByView()
+    ])
+
+    res.render('pages/index', {
+        stats,
+        hotBooks
+    })
 }))
 
 router.use('/setup', require('./setup'))
