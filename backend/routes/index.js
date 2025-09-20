1eb9c190d6ce81c174d05909de505bc4a9b27d4c
qianmoQ <shicheng@devlive.org>
2025-09-20 19:54:59 +0800

    feat: 添加书籍列表

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { asyncHandler } = require('../middleware/async-handler')
 const Book = require('../models/book')
+const PaginationHelper = require('../tools/pagination')
 const router = express.Router()
 
 router.get('/', asyncHandler(async (req, res) => {
@@ -12,6 +13,26 @@ router.get('/', asyncHandler(async (req, res) => {
     res.render('pages/index', {
         stats,
         hotBooks
+    })
+}))
+
+router.get('/books', asyncHandler(async (req, res) => {
+    const paginationParams = PaginationHelper.parseParams(req.query, {
+        defaultLimit: 24
+    })
+
+    const searchParams = {
+        is_public: 1,
+        status: 'published'
+    }
+
+    const bookResponse = await Book.findAllByConditions(paginationParams, searchParams)
+    delete searchParams.is_public
+    delete searchParams.status
+
+    res.render('pages/book/index', {
+        data: bookResponse.data,
+        pagination: bookResponse.pagination
     })
 }))
 
