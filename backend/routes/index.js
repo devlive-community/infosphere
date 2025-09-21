d60de55bd45c2886beb61a49a6fc2d22cd411ce4
qianmoQ <shicheng@devlive.org>
2025-09-21 10:40:19 +0800

    feat: 添加用户书籍跳转

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,8 +4,10 @@ const Book = require('../models/book')
 const router = express.Router()
 
 router.get('/', asyncHandler(async (req, res) => {
+    const searchParams = { is_public: 1, status: 'published' }
+
     const [stats, hotBooks] = await Promise.all([
-        Book.summaryByUser(),
+        Book.summaryByConditions(searchParams),
         Book.findTop6ByView()
     ])
 
